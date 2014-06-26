e7ffe70ed170bf325f550330e0f7cdf144839a86
Thomas Portelange <thomas.portelange@boomerang.be>
2014-06-26 17:12:27 +0200

    move grid settings to settings tab

--- code/GSCSiteTree.php
+++ code/GSCSiteTree.php
@@ -1,38 +1,38 @@
 <?php
-class GSCSiteTree extends DataExtension{
-	
+
+class GSCSiteTree extends DataExtension {
+
 	protected $splitcontent = null;
-	
 	public static $db = array(
 		"GridLayout" => "Text"
 	);
-	
 	public static $casting = array(
 		"GridContent" => "HTMLText", //prevents save error
 	);
-	
-	function updateCMSFields(FieldList $fields){
+
+	public function updateSettingsFields(FieldList $fields) {
 		$fields->addFieldToTab("Root.Grid", TextareaField::create("GridLayout"));
-		
-		if($this->owner->GridLayout){
-			$fields->addFieldToTab("Root.Main",
-				GridSelectionField::create("GridPos","Content Position",$this->owner->GridLayout)
-			,"Content");
-			$fields->addFieldToTab("Root.Main",
-				HtmlEditorField::create("GridContent","Content")->setAttribute("data-gscdelimiter", GSCRenderer::$defaultdelimiter)
-			,"Content");
-			$fields->addFieldToTab("Root.Main", GSCHiddenContentField::create("Content"),"GridContent");
+		return $fields;
+	}
+
+	function updateCMSFields(FieldList $fields) {
+		if ($this->owner->GridLayout) {
+			$fields->addFieldToTab("Root.Main", GridSelectionField::create("GridPos", "Content Position", $this->owner->GridLayout)
+					, "Content");
+			$fields->addFieldToTab("Root.Main", HtmlEditorField::create("GridContent", "Content")->setAttribute("data-gscdelimiter", GSCRenderer::$defaultdelimiter)
+					, "Content");
+			$fields->addFieldToTab("Root.Main", GSCHiddenContentField::create("Content"), "GridContent");
 			Requirements::javascript("gridstructuredcontent/javascript/gridedit.js");
 		}
 	}
-	
-	function GridContent($pos = null){
+
+	function GridContent($pos = null) {
 		$renderer = new GSCRenderer($this->owner->GridLayout, $this->owner->Content);
 		return $renderer->render();
 	}
-	
-	function contentcontrollerInit(){
+
+	function contentcontrollerInit() {
 		$this->owner->Content = $this->GridContent();
 	}
-	
+
 }
