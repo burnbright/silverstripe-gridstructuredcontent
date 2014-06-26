a126b17d7cb8a5811f0f91b2c891f53a55277d54
Thomas Portelange <thomas.portelange@boomerang.be>
2014-06-26 17:28:29 +0200

    autoformat json

--- code/GSCSiteTree.php
+++ code/GSCSiteTree.php
@@ -11,11 +11,10 @@ class GSCSiteTree extends DataExtension {
 	);
 
 	public function updateSettingsFields(FieldList $fields) {
-		if(class_exists('CodeEditorField')) {
+		if (class_exists('CodeEditorField')) {
 			$editorfield = CodeEditorField::create("GridLayout");
 			$editorfield->setMode('json');
-		}
-		else {
+		} else {
 			$editorfield = TextareaField::create("GridLayout");
 		}
 		$fields->addFieldToTab("Root.Grid", $editorfield);
@@ -40,6 +39,80 @@ class GSCSiteTree extends DataExtension {
 
 	function contentcontrollerInit() {
 		$this->owner->Content = $this->GridContent();
+	}
+
+	/**
+	 * @link https://github.com/GerHobbelt/nicejson-php/blob/master/nicejson.php
+	 * @param string $json
+	 * @return string
+	 */
+	static function json_format($json) {
+		if (!is_string($json)) {
+			if (phpversion() && phpversion() >= 5.4) {
+				return json_encode($json, JSON_PRETTY_PRINT);
+			}
+			$json = json_encode($json);
+		}
+		$result = '';
+		$pos = 0;	  // indentation level
+		$strLen = strlen($json);
+		$indentStr = "\t";
+		$newLine = "\n";
+		$prevChar = '';
+		$outOfQuotes = true;
+
+		for ($i = 0; $i < $strLen; $i++) {
+			// Grab the next character in the string
+			$char = substr($json, $i, 1);
+
+			// Are we inside a quoted string?
+			if ($char == '"' && $prevChar != '\\') {
+				$outOfQuotes = !$outOfQuotes;
+			}
+			// If this character is the end of an element,
+			// output a new line and indent the next line
+			else if (($char == '}' || $char == ']') && $outOfQuotes) {
+				$result .= $newLine;
+				$pos--;
+				for ($j = 0; $j < $pos; $j++) {
+					$result .= $indentStr;
+				}
+			}
+			// eat all non-essential whitespace in the input as we do our own here and it would only mess up our process
+			else if ($outOfQuotes && false !== strpos(" \t\r\n", $char)) {
+				continue;
+			}
+
+			// Add the character to the result string
+			$result .= $char;
+			// always add a space after a field colon:
+			if ($char == ':' && $outOfQuotes) {
+				$result .= ' ';
+			}
+
+			// If the last character was the beginning of an element,
+			// output a new line and indent the next line
+			if (($char == ',' || $char == '{' || $char == '[') && $outOfQuotes) {
+				$result .= $newLine;
+				if ($char == '{' || $char == '[') {
+					$pos++;
+				}
+				for ($j = 0; $j < $pos; $j++) {
+					$result .= $indentStr;
+				}
+			}
+			$prevChar = $char;
+		}
+
+		return $result;
+	}
+
+	function onBeforeWrite() {
+		parent::onBeforeWrite();
+
+		if ($this->owner->GridLayout) {
+			$this->owner->GridLayout = self::json_format($this->owner->GridLayout);
+		}
 	}
 
 }
